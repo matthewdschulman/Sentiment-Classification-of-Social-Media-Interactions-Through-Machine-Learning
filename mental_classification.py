4226a20bb4cc2e51458ce9116031e3dd6ffc97ad
banisadr <bahram.banisadr@gamil.com>
2015-11-30 14:07:05 -0500

    begining mental health classifier

--- mental_classification.py
+++ mental_classification.py
@@ -16,7 +16,6 @@ from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn import metrics
 from sklearn.linear_model import SGDClassifier
-from tabulate import tabulate
 from sklearn.metrics import precision_recall_fscore_support
 from sklearn import svm
 from sklearn.preprocessing import label_binarize
@@ -37,16 +36,21 @@ def mapToNumericTargetValues(x):
 		return -5
 
 # import the sms-test-gold-A.tsv training data
-with open('data/sms/sms-test-gold-A.tsv','r') as f:
-	training=[x.strip().split('\t') for x in f]
+with open('data/mental_risk_keywords.csv','r') as f:
+	training=[x.strip().split(',') for x in f]
+
+
+print training
 
 np_training = np.array(training)
 
 # get the training data
-np_training_data = np_training[:,5]
+np_training_data = np_training[:,0]
 
 # get the training target values
-np_training_target = map(mapToNumericTargetValues, np_training[:,4])
+np_training_target = np_training[:,1]
+
+'''
 
 # Naive Bayes Classifier and fitting
 text_clf_bayes = Pipeline([('vect', CountVectorizer(lowercase=True, stop_words='english')),
@@ -56,17 +60,25 @@ text_clf_bayes = Pipeline([('vect', CountVectorizer(lowercase=True, stop_words='
 			 )
 text_clf_bayes = text_clf_bayes.fit(np_training_data, np_training_target)
 
+
 # Compute Naive Bayes predictions
 training_predicted_nb = text_clf_bayes.predict(np_training_data)
 training_accuracy_nb = np.mean(training_predicted_nb == np_training_target)
 print "The Training Accuracy for the Naive Bayes Classifier = {0}".format(training_accuracy_nb)
 
+
 # SVM Cosine Similarity Classifier and Fitting
 text_clf_svm = Pipeline([('vect', CountVectorizer(lowercase=True, stop_words='english')),
  			 ('tfidf', TfidfTransformer(norm='l2', sublinear_tf=True)),
- 			 ('clf', svm.SVC(kernel=sklearn.metrics.pairwise.linear_kernel, probability=True)), 
+ 			 ('clf', svm.SVC(kernel=sklearn.metrics.pairwise.linear_kernel, probability=True)),
  		        ]
  		       )
+'''
+
+text_clf_svm = Pipeline([('vect', CountVectorizer(stop_words = 'english')),
+	                     ('tfidf', TfidfTransformer(sublinear_tf=True, norm='l2')),
+	                     ('clf', svm.SVC(kernel=sklearn.metrics.pairwise.cosine_similarity, probability=True))])
+
 text_clf_svm = text_clf_svm.fit(np_training_data, np_training_target)
 
 # Compute SVM predictions
@@ -79,21 +91,21 @@ while 1:
 	print "Please enter a sentence to be classified:"
 	user_input = raw_input()
 	np_testing_data = [user_input]
-	testing_predictions_nb = text_clf_bayes.predict(np_testing_data)
-	testing_predictions_svm = text_clf_svm.predict(np_testing_data)
+	# testing_predictions_nb = text_clf_bayes.predict(np_testing_data)
+	testing_predictions_svm = text_clf_svm.predict_proba(np_testing_data)
 
 	# Print prediction for Naive Bayes
-	if testing_predictions_nb[0] == 1:
-		print "Naive Bayes predicts that your sentence is positive"
-	elif testing_predictions_nb[0] == -1:
-		print "Naive Bayes predicts that your sentence is negative"
-	elif testing_predictions_nb[0] == 0:
-		print "Naive Bayes predicts that your sentence is neutral"
+	# if testing_predictions_nb[0] == 1:
+	# 	print "Naive Bayes predicts that your sentence is positive"
+	# elif testing_predictions_nb[0] == -1:
+	# 	print "Naive Bayes predicts that your sentence is negative"
+	# elif testing_predictions_nb[0] == 0:
+	# 	print "Naive Bayes predicts that your sentence is neutral"
 
 	# Print prediction for SVM
-	if testing_predictions_svm[0] == 1:
-		print "SVM predicts that your sentence is positive\n"
-	elif testing_predictions_svm[0] == -1:
-		print "SVM Bayes predicts that your sentence is negative\n"
-	elif testing_predictions_svm[0] == 0:
-		print "SVM Bayes predicts that your sentence is neutral\n"
+	if testing_predictions_svm[0] > 0.5:
+		print "SVM predicts that your sentence is positive: {0}\n".format(testing_predictions_svm[0])
+	elif testing_predictions_svm[0] < 0.5:
+		print "SVM Bayes predicts that your sentence is negative: {0}\n".format(testing_predictions_svm[0])
+	else:
+		print "SVM Bayes prediction unknow\n"
